f15c02201ffbc0a784ec21a1cad6abb03b5073a6
Moon <1193040110@qq.com>
2023-11-28 16:34:05 +0800

    feat:新增注册用户验证

--- backend/urls.py
+++ backend/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('create_user', views.create_user)  # http://127.0.0.1:8000/api/create_user
 
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,29 @@
+from django.http import HttpResponse, JsonResponse
+import json
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
+from backend.models import Book, Community, User
+
 
 # Create your views here.
+def create_user(request):
+    res = {"code": 400, "message": "", "data": None}
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            user = User.objects.create(**data)
+            user.save()
+            res["code"] = 200
+            res["message"] = "success"
+        except Exception as e:
+            res["code"] = 500
+            res["message"] = "服务器错误：" + str(e)
+    else:
+        res["message"] = "请使用POST方法"
+    return JsonResponse(res)
+
+
+# 用户登录
+def log_in(request):
+    pass
